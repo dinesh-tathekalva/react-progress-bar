344368167b2b0cbc3015b6be4f0c87e361584c2f
dinesh-tathekalva <dinesh.tathekalva@gmail.com>
2025-10-05 23:06:48 -0500

    updated

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,15 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { MAX, MIN } from "../constants";
 
-const ProgressBar = ({value = 0}) => {
-const [percent, setPercent] = useState(value)
-useEffect(()=> {
-    setPercent(Math.min(100, Math.max(value, 0)))
-}, [])
-    return (
-         <div className="progress">
-            <span> {percent.toFixed()}% </span>
-         </div>
-    )
+export default function ProgressBar({ value = 0, onComplete = () => {} }) {
+  const [percent, setPercent] = useState(value);
+
+  useEffect(() => {
+    setPercent(Math.min(Math.max(value, MIN), MAX));
+
+    if (value >= MAX) {
+      onComplete();
+    }
+  }, [value]);
+
+  return (
+    <div className="progress">
+      <span
+        style={{
+          color: percent > 49 ? "white" : "black"
+        }}
+      >
+        {percent.toFixed()}%
+      </span>
+      <div
+        style={{
+          transform: `scaleX(${percent / MAX})`,
+          transformOrigin: "left"
+        }}
+        aria-valuemin={MIN}
+        aria-valuemax={MAX}
+        aria-valuenow={percent}
+        role="progressbar"
+      />
+    </div>
+  );
 }
-
-export default ProgressBar
